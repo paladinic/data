c23f9cab6dcb2f1a13172f1af79e203afb32e027
paladinic <clode.paladini@gmail.com>
2021-04-03 08:20:45 +0100

    editing ecom_data.R

--- ecom_data.R
+++ ecom_data.R
@@ -1,8 +1,8 @@
-# ecommerce = gtrendsR::gtrends("amazon")$interest_over_time %>% 
-#   mutate(hits = hits * 1000)
-# covid = gtrendsR::gtrends("covid")$interest_over_time
-# christmas = gtrendsR::gtrends("christmas")$interest_over_time
-# black_friday = gtrendsR::gtrends("black friday")$interest_over_time
+ecommerce = gtrendsR::gtrends("amazon")$interest_over_time %>%
+  mutate(hits = hits * 1000)
+covid = gtrendsR::gtrends("covid")$interest_over_time
+christmas = gtrendsR::gtrends("christmas")$interest_over_time
+black_friday = gtrendsR::gtrends("black friday")$interest_over_time
 
 
 df = rbind(ecommerce,
@@ -17,16 +17,22 @@ df = rbind(ecommerce,
   rownames_to_column("date")
 
 n = nrow(df)
-online_media = c(rep(0,n-5),10000,20000,10000,10000,0)
+online_media = c(rep(0,n-105),1000,2000,1000,1000,0,rep(0,100))
 offline_media =  c(rep(0,n-25),
-                   10000,3000,20000,1000,18000,
-                   2000,30000,2500,32000,10000,
+                   1000,300,2000,1000,1000,
+                   2000,10000,1000,1500,5000,
                    rep(0,15))
+promo =  c(rep(0,n-40),
+           1000,1000,1000,1000,1000,
+           2000,2000,2000,2000,2000,
+           rep(0,30))
 
-df = cbind(df,online_media,offline_media) %>% 
+df = cbind(df,online_media,offline_media,promo) %>% 
   mutate(amazon = amazon + 
-           online_media*2 + 
-           offline_media*3) %>% 
+           online_media*5 + 
+           promo*4 + 
+           offline_media*6) %>% 
   rename(ecommerce = amazon)
 
 df %>% write.csv(file = "c:/Users/44751/Desktop/github_data/data/ecomm_data.csv")
+
